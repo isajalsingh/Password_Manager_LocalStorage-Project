d2d56c3dfd9c8982dcc94c9592278da5494e06e9
Sajal Kumar Ujjwal <sajalkr2011@gmail.com>
2024-07-29 00:12:26 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-800 text-white flex flex-col justify-center items-center w-full'>
+         <div className='logo font-bold text-white text-l'>
+                    <span className='text-green-600'> &lt;</span>
+                    <span>Password</span>
+                    <span className='text-green-600'>Manager/&gt;</span>
+                </div>
+                <div className='text-white text-sm pb-1'>Created By Sajal Kumar Ujjwal</div>
+    </div>
+  )
+}
+
+export default Footer
